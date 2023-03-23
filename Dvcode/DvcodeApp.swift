cc9159f0f826c7003b9148ad6ea0cf516e7fefec
Kevin Peng <pengkevi@amazon.com>
2023-03-23 18:19:37 -0400

    Initial Commit

---
+++ Dvcode/DvcodeApp.swift
@@ -0,0 +1,17 @@
+//
+//  DvcodeApp.swift
+//  Dvcode
+//
+//  Created by Peng, Kevin [C] on 2023-03-23.
+//
+
+import SwiftUI
+
+@main
+struct DvcodeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
